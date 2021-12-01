8a9ce8d76731a98f7e6a6cd32ebd967e86071b95
Hopson97 <mhopson@hotmail.co.uk>
2021-12-01 06:54:02 +0000

    pydoc

--- day1/day1.py
+++ day1/day1.py
@@ -1,9 +1,15 @@
+# https://adventofcode.com/2021/day/1
+#
+
+
 def part1():
     """
-    >>> part1()
+    Counts the number of numerical increases in the input file
     """
     prev = 999999999999999999
     count = 0
+
+    #
     with open("day1/input.txt", "r") as f:
         for line in f:
             if int(line) > prev:
@@ -13,14 +19,24 @@ def part1():
 
 
 def part2():
+    """
+    Sums the previous 3 numbers in the input file and counts the number of sum increases
+    """
+    # Store all previous sums of 3 numbers
     prev = []
+
+    # Store the current sum of 3 numbers
     sums = []
+
+    # Store the current number of increases
     count = 0
     with open("day1/input.txt", "r") as f:
         for line in f:
             prev.append(int(line))
 
-            # sum the last 3 values in 'prev'
+            # When there are at least 3 previous numbers,
+            # sum the last 3 numbers and see if the sum is
+            # greater than the sum of the previous 3 numbers
             if len(prev) > 2:
                 sums.append(sum(prev[-3:]))
                 if len(sums) > 1 and sums[-1] > sums[-2]:
