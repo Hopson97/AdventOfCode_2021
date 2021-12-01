81b6b9ab5ff9f0c8773f43f1756a0e42e12f4692
Hopson97 <mhopson@hotmail.co.uk>
2021-12-01 06:50:16 +0000

    add some comments

--- day1/day1.py
+++ day1/day1.py
@@ -1,28 +1,26 @@
-
-
 def part1():
     """
     >>> part1()
     """
     prev = 999999999999999999
     count = 0
-    with open ("day1/input.txt", "r") as f:
-        for line in f:        
+    with open("day1/input.txt", "r") as f:
+        for line in f:
             if int(line) > prev:
                 count += 1
             prev = int(line)
     return count
 
+
 def part2():
     prev = []
     sums = []
     count = 0
-    with open ("day1/input.txt", "r") as f:
-        for line in f:        
-            v = int(line)
-            prev.append(v)
+    with open("day1/input.txt", "r") as f:
+        for line in f:
+            prev.append(int(line))
 
-            #sum the last 3 values in 'prev'
+            # sum the last 3 values in 'prev'
             if len(prev) > 2:
                 sums.append(sum(prev[-3:]))
                 if len(sums) > 1 and sums[-1] > sums[-2]:
@@ -30,8 +28,7 @@ def part2():
     return count
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     count = part1()
     print(count)
 
